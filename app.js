4f925225184ccb02967c212169f2feee553511c4
Pepe Valdivia <jvaldivia@softweb.pe>
2019-07-21 17:33:50 -0500

    Acceso a base de datos SQL

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+const models = require('./configs/models');
 
 function respond(req, res, next) {
   console.log('2++++++++++++++++');
@@ -10,11 +11,19 @@ var server = restify.createServer();
 
 server.use(function(req, res, next) {
   console.warn('run for all routes!');
+  res.header('Server', 'Ubuntu');
   return next();
 });
 
+async function list(req, res, next){
+  var students = await models.Student.findAll({});
+  res.header('Content-Type', 'text/html; charset=utf-8');
+  res.send(JSON.stringify(students));
+  next();
+}
 
 server.get('/hello/:name', respond);
+server.get('/list', list);
 server.head('/hello/:name', respond);
 
 server.listen(8080, function() {
